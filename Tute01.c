4a30c6e4615fdf206d3d3d5f7b27c3deaf041f3c
IT21266096 <99247843+IT21266096@users.noreply.github.com>
2022-03-06 15:26:34 +0000

    Tute01 completed

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,19 @@
 #include <stdio.h>
 
 int main() {
+
+  int sub01, sub02;
+  float average;
+
+  printf("Enter mark_01 : ");
+  scanf("%d", &sub01);
+
+  printf("Enter mark_02 : ");
+  scanf("%d", &sub02);
+
+  average = (sub01 + sub02) / 2.0;
+
+  printf("Average : %2.f\n", average);
   
   return 0;
 }
